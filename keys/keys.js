98f3b391a0b0240cb287616d5348fd6f896e77cf
shiqiongqiong <454798960@qq.com>
2017-12-04 19:53:18 +0800

    Site updated: 2017-12-04 19:53:17

---
+++ keys/keys.js
@@ -0,0 +1,18 @@
+(function() {
+  var i; /*index*/
+  
+ 
+
+   
+  /*初始化list*/
+  function initList() {   
+    closeBtn();
+    closeElement();
+    ifChecked();
+  }
+  
+  /*初始化*/
+  function init() {
+    var addButton = document.getElementById("addButton"); 
+    initList();
+
